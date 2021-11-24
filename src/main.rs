59f98942fbbba086a7b468ac2bc178a834c9a2be
BeeMargarida <ms@platforme.com>
2021-11-24 22:54:39 +0000

    test: added unit tests

--- src/main.rs
+++ src/main.rs
@@ -33,3 +33,41 @@ fn rocket() -> _ {
         .manage(DashMap::<u32, String>::new())
         .mount("/", routes![shorten, redirect])
 }
+
+#[cfg(test)]
+mod tests {
+    use rocket::{local::blocking::Client, http::Status};
+    use super::rocket;
+
+    #[test]
+    fn valid_request() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        
+        let response = client.post("/api/shorten?url=https://duckduckgo.com").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+
+        let key: u32 = response.into_string().expect("body").parse().expect("valid u32");
+        
+        let response = client.get(format!("/{}", key)).dispatch();
+        assert_eq!(response.status(), Status::SeeOther);
+
+        let redirect = response.headers().get_one("Location").expect("location header");
+        assert_eq!(redirect, "https://duckduckgo.com")
+    }
+
+    #[test]
+    fn empty_url() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+
+        let response = client.post("/api/shorten?url=").dispatch();
+        assert_eq!(response.status(), Status::BadRequest);
+    }
+
+    #[test]
+    fn invalid_url() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+
+        let response = client.post("/wrong").dispatch();
+        assert_eq!(response.status(), Status::NotFound);
+    }
+}
